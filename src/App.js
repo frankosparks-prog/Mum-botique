8557cb5b93e03dfe3bd6b4e5a61eb41093d5e42c
frankosparks-prog <mainafrank400@gmail.com>
2025-04-30 00:42:48 +0300

    Running..

---
+++ src/App.js
@@ -0,0 +1,35 @@
+// src/App.js
+
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import ScrollToTop from "./components/scrollTop";
+
+import Home from "./components/Home";
+import About from "./components/About";
+import Products from "./components/Products";
+import Categories from "./components/Categories";
+import Contact from "./components/Contact";
+import NotFound from "./components/NotFound";
+
+const App = () => {
+  return (
+    <div className="font-sans">
+      <ScrollToTop />
+      <Navbar />
+
+      <main className="pt-24">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Nav.js
@@ -0,0 +1,13 @@
+// Root.js or just inline in index.js
+
+import React from 'react';
+import Navbar from './components/Navbar';
+
+export default function Nav() {
+  return (
+    <>
+      <Navbar />
+      {/* Add more layout here if needed */}
+    </>
+  );
+}
